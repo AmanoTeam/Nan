c6279a7a4d50ae135e64ec37069030e903998d4e
Kartatz <105828205+Kartatz@users.noreply.github.com>
2023-09-01 15:49:40 -0300

    Add missing target properties

---
+++ arm-remarkable-linux-gnueabihf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+declare extra_configure_flags='--with-cpu=cortex-a9 --with-fpu=neon --with-float=hard --with-mode=thumb'
+
+declare triplet='arm-remarkable-linux-gnueabihf'
+
+declare sysroot='https://web.archive.org/web/0if_/https://github.com/koreader/koxtoolchain/releases/latest/download/remarkable.zip'
