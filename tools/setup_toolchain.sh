7d8dc86810d530ebb65a0246b50e9cdf7fe11076
Kartatz <105828205+Kartatz@users.noreply.github.com>
2023-09-01 17:19:59 -0300

    Fix toolchain name

--- tools/setup_toolchain.sh
+++ tools/setup_toolchain.sh
@@ -2,27 +2,27 @@
 
 set -eu
 
-declare -r KEL_HOME='/tmp/kel-toolchain'
+declare -r NAN_HOME='/tmp/nan-toolchain'
 
-if [ -d "${KEL_HOME}" ]; then
-	PATH+=":${KEL_HOME}/bin"
-	export KEL_HOME \
+if [ -d "${NAN_HOME}" ]; then
+	PATH+=":${NAN_HOME}/bin"
+	export NAN_HOME \
 		PATH
 	return 0
 fi
 
-declare -r KEL_CROSS_TAG="$(jq --raw-output '.tag_name' <<< "$(curl --retry 10 --retry-delay 3 --silent --url 'https://api.github.com/repos/AmanoTeam/Kel/releases/latest')")"
-declare -r KEL_CROSS_TARBALL='/tmp/kel.tar.xz'
-declare -r KEL_CROSS_URL="https://github.com/AmanoTeam/Kel/releases/download/${KEL_CROSS_TAG}/x86_64-unknown-linux-gnu.tar.xz"
+declare -r NAN_CROSS_TAG="$(jq --raw-output '.tag_name' <<< "$(curl --retry 10 --retry-delay 3 --silent --url 'https://api.github.com/repos/AmanoTeam/Nan/releases/latest')")"
+declare -r NAN_CROSS_TARBALL='/tmp/nan.tar.xz'
+declare -r NAN_CROSS_URL="https://github.com/AmanoTeam/Nan/releases/download/${NAN_CROSS_TAG}/x86_64-unknown-linux-gnu.tar.xz"
 
-curl --connect-timeout '10' --retry '15' --retry-all-errors --fail --silent --location --url "${KEL_CROSS_URL}" --output "${KEL_CROSS_TARBALL}"
-tar --directory="$(dirname "${KEL_CROSS_TARBALL}")" --extract --file="${KEL_CROSS_TARBALL}"
+curl --connect-timeout '10' --retry '15' --retry-all-errors --fail --silent --location --url "${NAN_CROSS_URL}" --output "${NAN_CROSS_TARBALL}"
+tar --directory="$(dirname "${NAN_CROSS_TARBALL}")" --extract --file="${NAN_CROSS_TARBALL}"
 
-rm "${KEL_CROSS_TARBALL}"
+rm "${NAN_CROSS_TARBALL}"
 
-mv '/tmp/kel' "${KEL_HOME}"
+mv '/tmp/nan' "${NAN_HOME}"
 
-PATH+=":${KEL_HOME}/bin"
+PATH+=":${NAN_HOME}/bin"
 
-export KEL_HOME \
+export NAN_HOME \
 	PATH
