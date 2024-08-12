8b7a9639eb73e5de9217e2f98313ad1e0a90264a
Kartatz <105828205+Kartatz@users.noreply.github.com>
2024-08-12 20:47:09 -0300

    Update to GCC 14.2.0

--- arm-remarkable-linux-gnueabihf.sh
+++ arm-remarkable-linux-gnueabihf.sh
@@ -4,4 +4,5 @@ declare extra_configure_flags='--with-cpu=cortex-a9 --with-fpu=neon --with-float
 
 declare triplet='arm-remarkable-linux-gnueabihf'
 
-declare sysroot='https://web.archive.org/web/0if_/https://github.com/koreader/koxtoolchain/releases/latest/download/remarkable.zip'
+# https://web.archive.org/web/0if_/https://github.com/koreader/koxtoolchain/releases/latest/download/remarkable.zip
+declare sysroot='https://github.com/koreader/koxtoolchain/releases/latest/download/remarkable.zip'
